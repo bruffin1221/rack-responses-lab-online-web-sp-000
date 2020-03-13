d06947829c72508e5f78121a6d181af1a1e263f1
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-13 18:32:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,14 @@
-requite
 class Application
 
   def call(env)
     resp = Rack::Response.new
-
-    if num_1==num_2 && num_2==num_3
-       resp.write "You Win"
-     else
-       resp.write "You Lose"
-     end
-
+    t=Time.new()
+    if t.hour<12
+      resp.write  "Good Morning"
+    elsif t.hour>12
+      resp.write "Good Afternoon"
+   end
      resp.finish
    end
 
  end
-
- t=Time.new(2020, 11, 3, 4, 30)
- t
