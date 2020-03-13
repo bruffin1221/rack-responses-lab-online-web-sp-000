7e14e088be68bde1fe581142e2a585c341b3aeef
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-13 18:39:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     t=Time.new()
-    if t.hour<12
+    if t.hour<=12
       resp.write  "/Morning/i"
     elsif t.hour>12
       resp.write "Good Afternoon"
